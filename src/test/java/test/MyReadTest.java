cd125bedbe13f1543cea275124a04d45ba96599c
Ryan Miao <ryan.miao@nf-3.com>
2020-02-24 19:02:42 +0800

    line

--- src/test/java/test/MyReadTest.java
+++ src/test/java/test/MyReadTest.java
@@ -22,12 +22,18 @@ public class MyReadTest {
           // 我们可以直接全部取出excel的数据， 然后自己自定义处理就好。 针对excel数据不大的时候可以这么干， 大数据也不会复杂了
         // 这里 也可以不指定class，返回一个list，然后读取第一个sheet 同步读取会自动finish
         List<Map<Integer, String>> listMap = EasyExcel.read(fileName).sheet().doReadSync();
+        int lineNum = 0;
         for (Map<Integer, String> data : listMap) {
             // 返回每条数据的键值对 表示所在的列 和所在列的值
-            log.info("读取到数据:{}", JSON.toJSONString(data));
+            lineNum = lineNum + 1;
+
+
+            log.info("\n读取到数据:, 第{}行: {}", lineNum, JSON.toJSONString(data));
+
         }
 
         // 如果数据量特别大， 则可以一行行去获取，参见 @demo.TestRead.noModleRead
+
 
     }
 }
