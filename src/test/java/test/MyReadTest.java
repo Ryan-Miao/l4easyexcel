8ee47091af33561fa8b7d12c04efea9107fc8fc4
Ryan Miao <ryan.miao@nf-3.com>
2020-02-25 19:16:04 +0800

    others conf

--- src/test/java/test/MyReadTest.java
+++ src/test/java/test/MyReadTest.java
@@ -1,12 +1,12 @@
 package test;
 
 import com.alibaba.excel.EasyExcel;
-import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import util.TestFileUtil;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,35 +18,57 @@ import java.util.Map;
 public class MyReadTest {
 
     @Test
-    public void testReadAll() {
+    public void testReadBase() {
         String fileName = TestFileUtil.getPath() + "test" + File.separator + "test.xlsx";
-        // 我们可以直接全部取出excel的数据， 然后自己自定义处理就好。 针对excel数据不大的时候可以这么干， 大数据也不会复杂了
-        // 这里 也可以不指定class，返回一个list，然后读取第一个sheet 同步读取会自动finish
         List<Map<Integer, String>> listMap = EasyExcel.read(fileName).sheet().doReadSync();
         /**
          * 行数:  {列数: 变量名称 }
          */
         LinkedHashMap<String, LinkedHashMap<String, String>> baseVar = TestConstant.baseVar();
+        /**
+         * 解析完毕后的变量 {varName: value}
+         */
+        Map<String, String> readValMap = new HashMap<>();
         int lineNum = 1;
         for (Map<Integer, String> data : listMap) {
             // 返回每条数据的键值对 表示所在的列 和所在列的值
             lineNum = lineNum + 1;
-//            log.info("\n读取到数据:, 第{}行: {}", lineNum, JSON.toJSONString(data));
+//            log.debug("\n读取到数据:, 第{}行: {}", lineNum, JSON.toJSONString(data));
             LinkedHashMap<String, String> line = baseVar.get("L-" + lineNum);
-            if (line != null) {
-                for (Map.Entry<Integer, String> entry : data.entrySet()) {
-                    Integer columnId = entry.getKey();
-                    String cellVar = line.get("C-" + columnId);
-                    if (cellVar != null) {
-                        log.info("第{} 行, 第 {} 列, {}={}", lineNum, columnId, cellVar, entry.getValue());
-                    }
-                }
+            if (line == null) {
+                continue;
             }
 
+            for (Map.Entry<Integer, String> entry : data.entrySet()) {
+                Integer columnId = entry.getKey();
+                String cellVar = line.get("C-" + columnId);
+                if (cellVar == null) {
+                    continue;
+                }
+                if (log.isDebugEnabled()) {
+                    log.debug("第{} 行, 第 {} 列, {}={}", lineNum, columnId, cellVar, entry.getValue());
+                }
+                readValMap.put(cellVar, entry.getValue());
+            }
 
         }
 
-        // 如果数据量特别大， 则可以一行行去获取，参见 @demo.TestRead.noModleRead
+        log.debug("读取到的变量为:{}", readValMap);
+
+
+    }
+
+    @Test
+    public void testReadOthers() {
+        String fileName = TestFileUtil.getPath() + "test" + File.separator + "test.xlsx";
+        List<Map<Integer, String>> listMap = EasyExcel.read(fileName).sheet(1).doReadSync();
+        // L3 C2 农民平均收入
+        // L5
+        //  C0 名称
+        //  C1 规模
+        //  C2 频率
+        //  C3 发放单位
+        //  C4 发放形式
 
 
     }
